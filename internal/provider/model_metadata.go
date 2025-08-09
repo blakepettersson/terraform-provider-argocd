9892bcccd797336cae462927db96734084dc0861
Blake Pettersson <blake.pettersson@gmail.com>
2025-08-09 23:13:44 +0200

    wip
    Signed-off-by: Blake Pettersson <blake.pettersson@gmail.com>

--- internal/provider/model_metadata.go
+++ internal/provider/model_metadata.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/argoproj-labs/terraform-provider-argocd/internal/utils"
 	"github.com/argoproj-labs/terraform-provider-argocd/internal/validators"
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
-	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -29,65 +29,6 @@ func objectMetaSchemaAttribute(objectName string, computed bool) schema.Attribut
 	return schema.SingleNestedAttribute{
 		MarkdownDescription: "Standard Kubernetes object metadata. For more info see the [Kubernetes reference](https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#metadata).",
 		Required:            true,
-		Attributes: map[string]schema.Attribute{
-			"name": schema.StringAttribute{
-				MarkdownDescription: fmt.Sprintf("Name of the %s, must be unique. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/identifiers#names", objectName),
-				Required:            true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-				Validators: []validator.String{
-					validators.IsDNSSubdomain(),
-				},
-			},
-			"namespace": schema.StringAttribute{
-				MarkdownDescription: fmt.Sprintf("Namespace of the %s, must be unique. Cannot be updated. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/", objectName),
-				Optional:            true,
-				Computed:            computed,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-				Validators: []validator.String{
-					validators.IsDNSSubdomain(),
-				},
-			},
-			"annotations": schema.MapAttribute{
-				MarkdownDescription: "An unstructured key value map stored with the cluster secret that may be used to store arbitrary metadata. More info: http://kubernetes.io/docs/user-guide/annotations",
-				Computed:            computed,
-				Optional:            !computed,
-				ElementType:         types.StringType,
-				Validators: []validator.Map{
-					validators.MetadataAnnotations(),
-				},
-			},
-			"labels": schema.MapAttribute{
-				MarkdownDescription: "Map of string keys and values that can be used to organize and categorize (scope and select) the cluster secret. May match selectors of replication controllers and services. More info: http://kubernetes.io/docs/user-guide/labels",
-				Computed:            computed,
-				Optional:            !computed,
-				ElementType:         types.StringType,
-				Validators: []validator.Map{
-					validators.MetadataLabels(),
-				},
-			},
-			"generation": schema.Int64Attribute{
-				MarkdownDescription: "A sequence number representing a specific generation of the desired state.",
-				Computed:            true,
-			},
-			"resource_version": schema.StringAttribute{
-				MarkdownDescription: fmt.Sprintf("An opaque value that represents the internal version of this %s that can be used by clients to determine when %s has changed. Read more: https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency", objectName, objectName),
-				Computed:            true,
-			},
-			"uid": schema.StringAttribute{
-				MarkdownDescription: fmt.Sprintf("The unique in time and space value for this %s. More info: http://kubernetes.io/docs/user-guide/identifiers#uids", objectName),
-				Computed:            true,
-			},
-		},
-	}
-}
-
-func objectMetaSchemaBlock(objectName string, computed bool) schema.Block {
-	return schema.SingleNestedBlock{
-		MarkdownDescription: "Standard Kubernetes object metadata. For more info see the [Kubernetes reference](https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#metadata).",
 		Attributes: map[string]schema.Attribute{
 			"name": schema.StringAttribute{
 				MarkdownDescription: fmt.Sprintf("Name of the %s, must be unique. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/identifiers#names", objectName),
